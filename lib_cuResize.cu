8a0b472265760c314d0be6f5efcd007e7e7f242a
royinx <royfork@hotmail.com>
2023-08-12 00:46:52 -0400

    feat: cupy uchar3 support

--- lib_cuResize.cu
+++ lib_cuResize.cu
@@ -1,6 +1,6 @@
 extern "C"{
-#define TILE_DIM 32
-#define BLOCK_ROWS 8
+#define MAX_WIDTH 3840 // 7680 3840 1920
+
 
 __device__ float lerp1d(int a, int b, float w)
 {
@@ -12,7 +12,7 @@ __device__ float lerp2d(int f00, int f01, int f10, int f11,
 {
     centroid_w = (1 + lroundf(centroid_w) - centroid_w)/2;
     centroid_h = (1 + lroundf(centroid_h) - centroid_h)/2;
-    
+
     float r0, r1, r;
     r0 = lerp1d(f00,f01,centroid_w);
     r1 = lerp1d(f10,f11,centroid_w);
@@ -21,16 +21,136 @@ __device__ float lerp2d(int f00, int f01, int f10, int f11,
     return r;
 }
 
-__global__ void cuResize(unsigned char* src_img, unsigned char* dst_img, 
-    const int src_h, const int src_w, 
+__global__ void cuResize_xyz(unsigned char* src_img, unsigned char* dst_img,
+    const int SRC_H, const int SRC_W,
+    const int DST_H, const int DST_W,
+    const float scale_h, const float scale_w)
+{
+    /*
+    Input:
+        src_img - NHWC
+        channel C, default = 3
+
+    Output:
+        dst_img - NHWC
+    */
+    if (DST_W < SRC_W & threadIdx.x>=SRC_W){return;}
+    const uchar3* src = (uchar3*)(src_img);
+    uchar3* dst = (uchar3*)(dst_img);
+
+    // coordinate dst pixel in src image
+    int dst_row_idx = blockIdx.x;
+    float centroid_h;
+    centroid_h = scale_h * (dst_row_idx + 0.5);
+    // printf("dst_row_idx %f %f, %d\n", centroid_h, scale_h , dst_row_idx );
+    // return;
+    // if (threadIdx.x>=DST_W){return;}
+    // printf("centroid_h %f\n", centroid_h);
+    int src_h_idx = lroundf(centroid_h)-1;
+    // printf("src_h_idx %d\n", src_h_idx);
+    // return;
+    if (src_h_idx<0){src_h_idx=0;}
+
+    int n = blockIdx.y; // batch number
+    __shared__ uchar3 srcTile[2][MAX_WIDTH];  // cache `2 src rows` for `1 dst row` pixel
+    int row_start;
+    int pix_idx;
+
+    for( int w = threadIdx.x ; w < SRC_W ; w+=blockDim.x){
+        pix_idx = n * SRC_H * SRC_W +   // move to the start of image in batch
+                  src_h_idx * SRC_W ;   // move to the start of row index of src image
+        // loop over 2 row image
+        for (int row = 0; row < 2; row++){
+            row_start = pix_idx + SRC_W * row;            // jump to next row
+            // printf("row %d , w %d , row_start %d,\t %d\n", row, w, src_h_idx, pix_idx);
+            // printf("(%d,) (%d,%d) | row %d , w %d | %d  %d | %d %d %d %d\n",
+            //         threadIdx.x, blockIdx.x , blockIdx.y ,
+            //         row, w,
+            //         pix_idx , src_h_idx * SRC_W,
+            //         row_start+w, src[row_start+w].x, src[row_start+w].y, src[row_start+w].z);
+            srcTile[row][w].x = src[row_start+w].x;
+            srcTile[row][w].y = src[row_start+w].y;
+            srcTile[row][w].z = src[row_start+w].z;
+            }
+    }
+    // for( int row = 0; row < 2; row++){
+    //     for( int w = threadIdx.x ; w < SRC_W ; w+=blockDim.x){
+    //         printf("x, %d %d %d\n", row, w , srcTile[row][w].x);
+    //         printf("y, %d %d %d\n", row, w , srcTile[row][w].y);
+    //         printf("z, %d %d %d\n", row, w , srcTile[row][w].z);
+    //     }
+    // }
+    __syncthreads();
+
+    // return;
+
+    // some overhead threads in each image process
+    // when thread idx in one image exceed one image size return;
+
+    long long pixel_idx = n * DST_H * DST_W +  // offset batch
+                          blockIdx.x * DST_W + // offset row(height)
+                          threadIdx.x;         // offset col(width)
+
+    // if(pixel_idx > DST_H * DST_W){
+    //     printf("%d, %ld %d %d %d %d \n",
+    //     DST_H * DST_W, pixel_idx, blockIdx.x * DST_W , blockIdx.x , DST_W , threadIdx.x);
+    // }
+    // return;
+    uchar3 *f00, *f01, *f10, *f11;
+    float centroid_w;
+    for( int w = threadIdx.x ; w < DST_W ; w+=blockDim.x){
+
+        centroid_w = scale_w * (w + 0.5);
+        int src_w_idx = lroundf(centroid_w)-1;
+        if (src_w_idx<0){src_w_idx=0;}
+
+        // loop over 2 row image
+
+        f00 = &srcTile[0][src_w_idx];
+        f01 = &srcTile[0][src_w_idx+1];
+        f10 = &srcTile[1][src_w_idx];
+        f11 = &srcTile[1][src_w_idx+1];
+
+        // printf("%d %d %d %d \n", srcTile[row][src_w_idx].x, srcTile[row][src_w_idx+1].x, srcTile[row+1][w].x, srcTile[row+1][src_w_idx+1].x);
+
+        if (src_w_idx+1>=SRC_W){f01 = f00; f11 = f10;}
+        if (src_h_idx+1>=SRC_H){f10 = f00; f11 = f01;}
+
+        // printf("x, %ld tidx.x %d\tw %d\tf: %d\t%d\t%d\t%d\n", pixel_idx, threadIdx.x, w, (*f00).x, (*f01).x, (*f10).x, (*f11).x);
+        // printf("y, %ld tidx.x %d\tw %d\tf: %d\t%d\t%d\t%d\n", pixel_idx, threadIdx.x, w, (*f00).y, (*f01).y, (*f10).y, (*f11).y);
+        // printf("z, %ld tidx.x %d\tw %d\tf: %d\t%d\t%d\t%d\n", pixel_idx, threadIdx.x, w, (*f00).z, (*f01).z, (*f10).z, (*f11).z);
+
+        dst[pixel_idx].x = (unsigned char) lroundf(lerp2d((*f00).x, (*f01).x, (*f10).x, (*f11).x, centroid_h, centroid_w));
+        dst[pixel_idx].y = (unsigned char) lroundf(lerp2d((*f00).y, (*f01).y, (*f10).y, (*f11).y, centroid_h, centroid_w));
+        dst[pixel_idx].z = (unsigned char) lroundf(lerp2d((*f00).z, (*f01).z, (*f10).z, (*f11).z, centroid_h, centroid_w));
+
+        // idx += w;
+
+        // if(pixel_idx > DST_H * DST_W){
+        // printf("%ld , %d , %d , %d\n", pixel_idx, threadIdx.x, w,blockDim.x );
+        // }
+        // gridDim.x * blockIdx.y + // DST_H * n
+        // blockIdx.x + // row number
+        // w;
+
+
+        // printf("%d \n",pixel_idx);
+        // centroid_w += scale_w*blockDim.x;
+        pixel_idx += blockDim.x;
+
+    }
+}
+
+__global__ void cuResize(unsigned char* src_img, unsigned char* dst_img,
+    const int src_h, const int src_w,
     const int dst_h, const int dst_w,
     const float scale_h, const float scale_w)
 {
-    /* 
-    Input: 
+    /*
+    Input:
         src_img - NHWC
-        channel C, default = 3 
-    
+        channel C, default = 3
+
     Output:
         dst_img - NHWC
     */
@@ -38,26 +158,26 @@ __global__ void cuResize(unsigned char* src_img, unsigned char* dst_img,
     // __shared__ float srcTile[TILE_DIM][TILE_DIM];
 
     int n = blockIdx.y; // batch number
-    int C = gridDim.z; // channel 
+    int C = gridDim.z; // channel
     int c = blockIdx.z; // channel number
-    long long idx = n * blockDim.x * gridDim.x * C + 
+    long long idx = n * blockDim.x * gridDim.x * C +
                     threadIdx.x * gridDim.x * C +
                     blockIdx.x * C+
                     c;
-    
+
     // some overhead threads in each image process
     // when thread idx in one image exceed one image size return;
-    if (idx%(blockDim.x * gridDim.x * C) >= dst_h* dst_w * C){return;} 
+    if (idx%(blockDim.x * gridDim.x * C) >= dst_h* dst_w * C){return;}
 
     int H = dst_h;
     int W = dst_w;
     int img_coor = idx % (dst_h*dst_w*C); //coordinate of one image, not idx of batch image
     int h = img_coor / (W*C);
-    int w = img_coor % (W*C)/C; 
+    int w = img_coor % (W*C)/C;
 
-    float centroid_h, centroid_w;  
+    float centroid_h, centroid_w;
     centroid_h = scale_h * (h + 0.5); // h w c -> x, y, z : 1080 , 1920 , 3
-    centroid_w = scale_w * (w + 0.5); // 
+    centroid_w = scale_w * (w + 0.5); //
 
     long long f00,f01,f10,f11;
 
@@ -66,8 +186,8 @@ __global__ void cuResize(unsigned char* src_img, unsigned char* dst_img,
     if (src_h_idx<0){src_h_idx=0;}
     if (src_w_idx<0){src_w_idx=0;}
 
-    f00 = n * src_h * src_w * C + 
-          src_h_idx * src_w * C + 
+    f00 = n * src_h * src_w * C +
+          src_h_idx * src_w * C +
           src_w_idx * C +
           c;
 
@@ -75,39 +195,40 @@ __global__ void cuResize(unsigned char* src_img, unsigned char* dst_img,
           src_h_idx * src_w * C +
           (src_w_idx+1) * C +
           c;
-    
+
     f10 = n * src_h * src_w * C +
           (src_h_idx+1) * src_w * C +
           src_w_idx * C +
           c;
-          
-    f11 = n * src_h * src_w * C + 
+
+    f11 = n * src_h * src_w * C +
           (src_h_idx+1) * src_w * C +
           (src_w_idx+1) * C +
           c;
     if (src_w_idx+1>=src_w){f01 = f00; f11 = f10;}
     if (src_h_idx+1>=src_h){f10 = f00; f11 = f01;}
-    int rs = lroundf(lerp2d(src_img[f00], src_img[f01], src_img[f10], src_img[f11], 
+    // printf("c %d, %d %d %d %d\n", c, src_img[f00], src_img[f01], src_img[f10], src_img[f11]);
+    int rs = lroundf(lerp2d(src_img[f00], src_img[f01], src_img[f10], src_img[f11],
         centroid_h, centroid_w));
-
-    long long dst_idx = n * (H * W * C) + 
+    // printf("%d\n", rs);
+    long long dst_idx = n * (H * W * C) +
                     h * (W * C) +
                     w * C +
                     c;
 
     dst_img[dst_idx] = (unsigned char)rs;
 }
-__global__ void cuResize_fp(unsigned char* src_img, float* dst_img, 
-    const int src_h, const int src_w, 
+__global__ void cuResize_fp(unsigned char* src_img, float* dst_img,
+    const int src_h, const int src_w,
     const int dst_h, const int dst_w,
     const float scale_h, const float scale_w,
     const bool normalise)
 {
-    /* 
-    Input: 
+    /*
+    Input:
         src_img - NHWC
-        channel C, default = 3 
-    
+        channel C, default = 3
+
     Output:
         dst_img - NHWC
     */
@@ -115,26 +236,26 @@ __global__ void cuResize_fp(unsigned char* src_img, float* dst_img,
     // __shared__ float srcTile[TILE_DIM][TILE_DIM];
 
     int n = blockIdx.y; // batch number
-    int C = gridDim.z; // channel 
+    int C = gridDim.z; // channel
     int c = blockIdx.z; // channel number
-    long long idx = n * blockDim.x * gridDim.x * C + 
+    long long idx = n * blockDim.x * gridDim.x * C +
               threadIdx.x * gridDim.x * C +
               blockIdx.x * C+
               c;
-    
+
     // some overhead threads in each image process
     // when thread idx in one image exceed one image size return;
-    if (idx%(blockDim.x * gridDim.x * C) >= dst_h* dst_w * C){return;} 
+    if (idx%(blockDim.x * gridDim.x * C) >= dst_h* dst_w * C){return;}
 
     int H = dst_h;
     int W = dst_w;
     int img_coor = idx % (dst_h*dst_w*C); //coordinate of one image, not idx of batch image
     int h = img_coor / (W*C);
-    int w = img_coor % (W*C)/C; 
+    int w = img_coor % (W*C)/C;
 
-    float centroid_h, centroid_w;  
+    float centroid_h, centroid_w;
     centroid_h = scale_h * (h + 0.5); // h w c -> x, y, z : 1080 , 1920 , 3
-    centroid_w = scale_w * (w + 0.5); // 
+    centroid_w = scale_w * (w + 0.5); //
 
     long long f00,f01,f10,f11;
 
@@ -143,8 +264,8 @@ __global__ void cuResize_fp(unsigned char* src_img, float* dst_img,
     if (src_h_idx<0){src_h_idx=0;}
     if (src_w_idx<0){src_w_idx=0;}
 
-    f00 = n * src_h * src_w * C + 
-          src_h_idx * src_w * C + 
+    f00 = n * src_h * src_w * C +
+          src_h_idx * src_w * C +
           src_w_idx * C +
           c;
 
@@ -152,25 +273,24 @@ __global__ void cuResize_fp(unsigned char* src_img, float* dst_img,
           src_h_idx * src_w * C +
           (src_w_idx+1) * C +
           c;
-    
+
     f10 = n * src_h * src_w * C +
           (src_h_idx+1) * src_w * C +
           src_w_idx * C +
           c;
-          
-    f11 = n * src_h * src_w * C + 
+
+    f11 = n * src_h * src_w * C +
           (src_h_idx+1) * src_w * C +
           (src_w_idx+1) * C +
           c;
-    
-    // printf("%ld, %ld, %ld, %ld \n", f00, f01, f10, f11);
+
     if (src_w_idx+1>=src_w){f01 = f00; f11 = f10;}
     if (src_h_idx+1>=src_h){f10 = f00; f11 = f01;}
 
-    int rs = lroundf(lerp2d(src_img[f00], src_img[f01], src_img[f10], src_img[f11], 
+    int rs = lroundf(lerp2d(src_img[f00], src_img[f01], src_img[f10], src_img[f11],
         centroid_h, centroid_w));
 
-    long long dst_idx = n * (H * W * C) + 
+    long long dst_idx = n * (H * W * C) +
                     h * (W * C) +
                     w * C +
                     c;
---
+++ resize_formated.py
@@ -0,0 +1,134 @@
+# pylint: disable=line-too-long, invalid-name, too-many-locals, raising-bad-type, c-extension-no-member, redefined-outer-name
+import cv2
+import cupy as cp
+import numpy as np
+from line_profiler import LineProfiler
+
+with open('lib_cuResize.cu', 'r', encoding="utf-8") as reader:
+    module = cp.RawModule(code=reader.read())
+
+cuResizeKer = module.get_function("cuResize_xyz")
+profile = LineProfiler()
+
+@profile
+def cuda_resize(inputs: cp.ndarray, # src: (N,H,W,C)
+                shape: tuple, # (dst_h, dst_w)
+                out: cp.ndarray=None, # dst: (N,H,W,C)
+                pad: bool=True):
+    """
+    to optimise with shared memory
+    block = (1024, )  # 1024 threads per block , to loop a row for dst row, with MAX_WIDTH 7680 (8K)
+    grid = (dst_h,N)  #
+    """
+    out_dtype = cp.uint8
+
+    N, src_h, src_w, C = inputs.shape
+    assert C == 3 # resize kernel only accept 3 channel tensors.
+    dst_h, dst_w = shape
+
+    if len(shape)!=2:
+        raise "cuda resize target shape must be (h,w)"
+    if out:
+        assert out.dtype == out_dtype
+        assert out.shape[1] == dst_h
+        assert out.shape[2] == dst_w
+
+    resize_scale = 1
+    left_pad = 0
+    top_pad = 0
+    if pad:
+        padded_batch = cp.zeros((N, dst_h, dst_w, C), dtype=out_dtype)
+        if src_h / src_w > dst_h / dst_w:
+            resize_scale = dst_h / src_h
+            ker_h = dst_h
+            ker_w = int(src_w * resize_scale)
+            left_pad = int((dst_w - ker_w) / 2)
+        else:
+            resize_scale = dst_w / src_w
+            ker_h = int(src_h * resize_scale)
+            ker_w = dst_w
+            top_pad = int((dst_h - ker_h) / 2)
+    else:
+        ker_h = dst_h
+        ker_w = dst_w
+
+    shape = (N, ker_h, ker_w, C)
+    if not out:
+        out = cp.empty(tuple(shape),dtype = out_dtype)
+    # define kernel configs
+    block = (1024, )
+    grid  = (ker_h, N)
+    with cp.cuda.stream.Stream() as stream:
+        print(inputs.dtype, out.dtype ,
+              inputs.shape, out.shape,
+              src_h, src_w,
+              ker_h, ker_w,
+              cp.float32(src_h/ker_h), cp.float32(src_w/ker_w))
+
+        cuResizeKer(grid, block,
+                (inputs, out,
+                cp.int32(src_h), cp.int32(src_w),
+                cp.int32(ker_h), cp.int32(ker_w),
+                cp.float32(src_h/ker_h), cp.float32(src_w/ker_w)
+                )
+            )
+
+        if pad:
+            if src_h / src_w > dst_h / dst_w:
+                padded_batch[:, :, left_pad:left_pad + out.shape[2], :] = out
+            else:
+                padded_batch[:, top_pad:top_pad + out.shape[1], :, :] = out
+            padded_batch = cp.ascontiguousarray(padded_batch)
+        stream.synchronize()
+
+    if pad:
+        return resize_scale, top_pad, left_pad, padded_batch
+    return resize_scale, top_pad, left_pad, out
+
+
+
+def main(input_array: cp.ndarray, resize_shape:tuple):
+    input_array_gpu = cp.empty(shape=input_array.shape,dtype=input_array.dtype)
+
+    if isinstance(input_array, cp.ndarray): # DtoD
+        cp.cuda.runtime.memcpy(dst = int(input_array_gpu.data), # dst_ptr
+                                src = int(input_array.data), # src_ptr
+                                size=input_array.nbytes,
+                                kind=3) # 0: HtoH, 1: HtoD, 2: DtoH, 3: DtoD, 4: unified virtual addressing
+    elif isinstance(input_array, np.ndarray):
+        cp.cuda.runtime.memcpy(dst = int(input_array_gpu.data), # dst_ptr
+                                src = input_array.ctypes.data, # src_ptr
+                                size=input_array.nbytes,
+                                kind=1)
+
+    resize_scale, top_pad, left_pad, output_array = cuda_resize(input_array_gpu,
+                                                                resize_shape,
+                                                                pad=True) # N,W,H,C
+
+    return output_array, [resize_scale, top_pad, left_pad]
+
+if __name__ == "__main__":
+    # prepare data
+    batch = 50
+    img_batch = np.tile(cv2.resize(cv2.imread("trump.jpg"),
+                                #    (2,2)),
+                                   (2560,1080)),
+                        [batch,1,1,1])
+    img_batch[-1] = np.tile(cv2.resize(cv2.imread("rgba.png"),(2560,1080)),[1,1,1])
+    cv2.imwrite("input.jpg", cp.asnumpy(img_batch[0]))
+    output_array, _ = main(img_batch, (192,200))
+
+    # img_batch = cp.arange(1*2*1024*3, dtype=cp.uint8).reshape((1,2,1024,3))
+    # output_array, _ = main(img_batch, (1 ,1024))
+
+    # img_batch = cp.arange(1*4*4*3, dtype=cp.uint8).reshape((1,4,4,3))
+    # img_batch = cp.tile(img_batch, (1,1,1,1))
+    # output_array, _ = main(img_batch, (200,200))
+    print(output_array)
+
+    # block = (1024, )
+    # grid  = (dst_h, N)
+
+
+    for idx, img in enumerate(cp.asnumpy(output_array)):
+        cv2.imwrite(f"output_{idx}.jpg", img)
